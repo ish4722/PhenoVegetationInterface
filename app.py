5609294898e51ec893ccf3529f4e744b7308f8ce
Medix1075 <jindalmedhansh@gmail.com>
2024-12-10 13:44:34 +0530

    updated

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from backend.preprocessing.filters import apply_filters
 from backend.segmentation.model import predict_masks
 from backend.analysis.vegetation_indices import calculate_indices
 from backend.analysis.excel_writer import create_excel
+from backend.segmentation.clustering import perform_clustering
+from backend.segmentation.roi_processing import ROI_Points, ROI_processing
 import uuid  # To generate unique filenames
 from config import *
 
@@ -48,18 +50,27 @@ def process():
         filtered_images = apply_filters(filtered_images, filters)
 
         # Step 4: Predict Segmentation with Model
-        segmentation_results = predict_masks(MODEL_PATH,filtered_images)
+        MASKS = predict_masks(MODEL_PATH,filtered_images)
+
+        n_ROIs, points, ROI_area, scaled_height, scaled_width = ROI_Points(MASKS, filtered_images)
+
+        centroids, labels = perform_clustering(points, ROI_area, n_ROIs)
+
+        red, green, blue = ROI_processing(labels, points, ROI_area, scaled_height, scaled_width, filtered_images)
 
         # Step 5: Calculate Vegetation Indices
-        vegetation_data = calculate_indices(segmentation_results)
+        indices_list = []
+        for red, green, blue in zip(red, green, blue):
+            indices = calculate_indices(red, green, blue)
+            indices_list.append(indices)
 
         # Step 6: Generate Excel Report
         excel_path = os.path.join(OUTPUT_FOLDER, "vegetation_report.xlsx")
-        create_excel(vegetation_data, excel_path)
+        create_excel(indices_list, excel_path)
 
         # Step 7: Generate Graph
-        # graph_path = os.path.join(OUTPUT_FOLDER, "vegetation_graph.png")
-        # vegetation_data.plot_graph(output_path=graph_path)  # Assuming `plot_graph` is implemented in `vegetation_data`
+        graph_path = os.path.join(OUTPUT_FOLDER, "vegetation_graph.png")
+        # indices_list.plot_graph(output_path=graph_path)  # Assuming `plot_graph` is implemented in `vegetation_data`
 
         # Step 8: Return Files to User
         return jsonify({
@@ -73,9 +84,9 @@ def process():
 def download_excel(filename):
     return send_file(os.path.join(OUTPUT_FOLDER, filename), as_attachment=True)
 
-@app.route("/download/graph/<filename>")
-def download_graph(filename):
-    return send_file(os.path.join(OUTPUT_FOLDER, filename), mimetype="image/png")
+# @app.route("/download/graph/<filename>")
+# def download_graph(filename):
+#     return send_file(os.path.join(OUTPUT_FOLDER, filename), mimetype="image/png").
 
 if __name__ == "__main__":
     app.run(debug=True)
